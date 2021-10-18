e18956917803a7f18eaf0aaff8035c357813f3ba
Riandy Rahman Nugraha <riandyrn@gmail.com>
2021-10-18 09:24:15 +0300

    update the code to be same as in https://github.com/open-telemetry/opentelemetry-go-contrib/pull/986; simplify test structure;

--- middleware_test.go
+++ middleware_test.go
@@ -11,9 +11,13 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/oteltest"
 	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -149,4 +153,61 @@ func TestResponseWriterInterfaces(t *testing.T) {
 	}
 
 	router.ServeHTTP(w, r)
+}
+
+func ok(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func TestSDKIntegration(t *testing.T) {
+	sr := tracetest.NewSpanRecorder()
+	provider := sdktrace.NewTracerProvider()
+	provider.RegisterSpanProcessor(sr)
+
+	router := chi.NewRouter()
+	router.Use(Middleware("foobar", WithTracerProvider(provider)))
+	router.HandleFunc("/user/{id:[0-9]+}", ok)
+	router.HandleFunc("/book/{title}", ok)
+
+	r0 := httptest.NewRequest("GET", "/user/123", nil)
+	r1 := httptest.NewRequest("GET", "/book/foo", nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, r0)
+	router.ServeHTTP(w, r1)
+
+	require.Len(t, sr.Ended(), 2)
+	assertSpan(t, sr.Ended()[0],
+		"/user/{id:[0-9]+}",
+		trace.SpanKindServer,
+		attribute.String("http.server_name", "foobar"),
+		attribute.Int("http.status_code", http.StatusOK),
+		attribute.String("http.method", "GET"),
+		attribute.String("http.target", "/user/123"),
+		attribute.String("http.route", "/user/{id:[0-9]+}"),
+	)
+	assertSpan(t, sr.Ended()[1],
+		"/book/{title}",
+		trace.SpanKindServer,
+		attribute.String("http.server_name", "foobar"),
+		attribute.Int("http.status_code", http.StatusOK),
+		attribute.String("http.method", "GET"),
+		attribute.String("http.target", "/book/foo"),
+		attribute.String("http.route", "/book/{title}"),
+	)
+}
+
+func assertSpan(t *testing.T, span sdktrace.ReadOnlySpan, name string, kind trace.SpanKind, attrs ...attribute.KeyValue) {
+	assert.Equal(t, name, span.Name())
+	assert.Equal(t, trace.SpanKindServer, span.SpanKind())
+
+	got := make(map[attribute.Key]attribute.Value, len(span.Attributes()))
+	for _, a := range span.Attributes() {
+		got[a.Key] = a.Value
+	}
+	for _, want := range attrs {
+		if !assert.Contains(t, got, want.Key) {
+			continue
+		}
+		assert.Equal(t, got[want.Key], want.Value)
+	}
 }
