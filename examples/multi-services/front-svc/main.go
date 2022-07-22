6032a95eeba817a6e7d54059e5d90a3bb31d594d
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-07-23 06:43:42 +0700

    docs: finish instrumenting front service & add jaeger in docker-compose.yml;

--- examples/multi-services/front-svc/main.go
+++ examples/multi-services/front-svc/main.go
@@ -9,19 +9,39 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
-	"golang.org/x/net/context/ctxhttp"
+	"github.com/riandyrn/otelchi"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
-	envKeyBackServiceURL = "BACK_SERVICE_URL"
-	addr                 = ":8090"
+	envKeyBackServiceURL    = "BACK_SERVICE_URL"
+	envKeyJaegerEndpointURL = "JAEGER_ENDPOINT_URL"
+	addr                    = ":8090"
 )
 
 func main() {
+	// init tracer provider
+	tp, err := initTracerProvider("front-svc", os.Getenv(envKeyJaegerEndpointURL))
+	if err != nil {
+		log.Fatalf("unable to initialize tracer provider due: %v", err)
+	}
+	// set global tracer provider & text propagators
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	tracer := otel.Tracer("")
 	// define router
 	r := chi.NewRouter()
+	r.Use(otelchi.Middleware("front-svc", otelchi.WithChiRoutes(r)))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		name, err := getRandomName(r.Context())
+		name, err := getRandomName(r.Context(), tracer)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
@@ -29,24 +49,52 @@ func main() {
 		}
 		w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
 	})
+	// execute server
 	log.Printf("front service is listening on %v", addr)
-	err := http.ListenAndServe(addr, r)
+	err = http.ListenAndServe(addr, r)
 	if err != nil {
 		log.Fatalf("unable to execute server due: %v", err)
 	}
 }
 
-func getRandomName(ctx context.Context) (string, error) {
-	resp, err := ctxhttp.Get(ctx, http.DefaultClient, os.Getenv(envKeyBackServiceURL))
+func getRandomName(ctx context.Context, tracer trace.Tracer) (string, error) {
+	ctx, span := tracer.Start(ctx, "getRandomName")
+	defer span.End()
+
+	resp, err := otelhttp.Get(ctx, os.Getenv(envKeyBackServiceURL))
 	if err != nil {
-		return "", fmt.Errorf("unable to execute http request due: %w", err)
+		err = fmt.Errorf("unable to execute http request due: %w", err)
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+		return "", err
 	}
 	defer resp.Body.Close()
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("unable to read response data due: %w", err)
+		err = fmt.Errorf("unable to read response data due: %w", err)
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+		return "", err
 	}
 
 	return string(data), nil
+}
+
+func initTracerProvider(svcName, jaegerEndpoint string) (*sdktrace.TracerProvider, error) {
+	// create jaeger exporter
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(jaegerEndpoint)))
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize exporter due: %w", err)
+	}
+	// initialize tracer provider
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(svcName),
+		)),
+	)
+	return tp, nil
 }
--- examples/multi-services/go.mod
+++ examples/multi-services/go.mod
@@ -4,5 +4,19 @@ go 1.18
 
 require (
 	github.com/go-chi/chi/v5 v5.0.7
-	golang.org/x/net v0.0.0-20220708220712-1185a9018129
+	github.com/riandyrn/otelchi v0.4.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.33.0
+	go.opentelemetry.io/otel v1.8.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.8.0
+	go.opentelemetry.io/otel/sdk v1.8.0
+	go.opentelemetry.io/otel/trace v1.8.0
+)
+
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/contrib v1.0.0 // indirect
+	go.opentelemetry.io/otel/metric v0.31.0 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
