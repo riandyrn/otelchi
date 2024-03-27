de37ffbfd057ff64b739119feb9f2afb83e71a9f
Riandy Rahman Nugraha <>
2024-03-28 00:44:56 +0700

    ci: use single version of go but execute the CI on multi OS for proof of concept;

--- test/infras/entrypoint.sh
+++ test/infras/entrypoint.sh
@@ -3,17 +3,6 @@
 # Terminate script immediately on any errors
 set -e
 
-# Define Go versions
-go_versions=${GO_VERSIONS}
-
-echo ${GO_VERSIONS}
-
-# Iterate over each version
-for version in $go_versions; do
-    # Set Go version using gobrew
-    gobrew use "$version"
-    
-    # Build and test
-    go build .
-    go test ./...
-done
+# Build and test
+go build .
+go test ./...
--- test/infras/install_go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Define Go versions
-go_versions=${GO_VERSIONS}
-
-# Install each Go version using gobrew
-for version in $go_versions; do
-    gobrew install "$version"
-done
