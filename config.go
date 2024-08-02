35f6bac426b9d6141ffa07abf6fdc9a2c8963a38
Riandy Rahman Nugraha <riandyrn@users.noreply.github.com>
2024-08-03 06:01:47 +0700

    Revert "feat: add response header for trace sample status"
    This reverts commit 17fe0df40868e19daba190b931b4ee54e2a9f4d8.

--- config.go
+++ config.go
@@ -8,21 +8,17 @@ import (
 	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
-const (
-	defaultTraceIDResponseHeaderKey      = "X-Trace-Id"
-	defaultTraceSampledResponseHeaderKey = "X-Trace-Sampled"
-)
+const defaultTraceResponseHeaderKey = "X-Trace-Id"
 
 // config is used to configure the mux middleware.
 type config struct {
-	TracerProvider           oteltrace.TracerProvider
-	Propagators              propagation.TextMapPropagator
-	ChiRoutes                chi.Routes
-	RequestMethodInSpanName  bool
-	Filters                  []Filter
-	TraceIDResponseHeaderKey string
-	TraceSampledResponseKey  string
-	PublicEndpointFn         func(r *http.Request) bool
+	TracerProvider          oteltrace.TracerProvider
+	Propagators             propagation.TextMapPropagator
+	ChiRoutes               chi.Routes
+	RequestMethodInSpanName bool
+	Filters                 []Filter
+	TraceResponseHeaderKey  string
+	PublicEndpointFn        func(r *http.Request) bool
 }
 
 // Option specifies instrumentation configuration options.
@@ -36,7 +32,7 @@ func (o optionFunc) apply(c *config) {
 	o(c)
 }
 
-// Filter is a predicate used to determine whether a given [http.Request] should
+// Filter is a predicate used to determine whether a given http.request should
 // be traced. A Filter must return true if the request should be traced.
 type Filter func(*http.Request) bool
 
@@ -102,9 +98,9 @@ func WithFilter(filter Filter) Option {
 func WithTraceIDResponseHeader(headerKeyFunc func() string) Option {
 	return optionFunc(func(cfg *config) {
 		if headerKeyFunc == nil {
-			cfg.TraceIDResponseHeaderKey = defaultTraceIDResponseHeaderKey // use default trace header
+			cfg.TraceResponseHeaderKey = defaultTraceResponseHeaderKey // use default trace header
 		} else {
-			cfg.TraceIDResponseHeaderKey = headerKeyFunc()
+			cfg.TraceResponseHeaderKey = headerKeyFunc()
 		}
 	})
 }
@@ -142,7 +138,7 @@ func WithPublicEndpoint() Option {
 // incoming span context. Otherwise, the generated span will be set as the
 // child span of the incoming span context.
 //
-// Essentially it has the same functionality as [WithPublicEndpoint] but with
+// Essentially it has the same functionality as WithPublicEndpoint but with
 // more flexibility.
 func WithPublicEndpointFn(fn func(r *http.Request) bool) Option {
 	return optionFunc(func(cfg *config) {
--- middleware.go
+++ middleware.go
@@ -2,7 +2,6 @@ package otelchi
 
 import (
 	"net/http"
-	"strconv"
 	"sync"
 
 	"github.com/felixge/httpsnoop"
@@ -24,9 +23,7 @@ const (
 // requests. The serverName parameter should describe the name of the
 // (virtual) server handling the request.
 func Middleware(serverName string, opts ...Option) func(next http.Handler) http.Handler {
-	cfg := config{
-		TraceSampledResponseKey: defaultTraceSampledResponseHeaderKey,
-	}
+	cfg := config{}
 	for _, opt := range opts {
 		opt.apply(&cfg)
 	}
@@ -43,31 +40,29 @@ func Middleware(serverName string, opts ...Option) func(next http.Handler) http.
 
 	return func(handler http.Handler) http.Handler {
 		return traceware{
-			serverName:               serverName,
-			tracer:                   tracer,
-			propagators:              cfg.Propagators,
-			handler:                  handler,
-			chiRoutes:                cfg.ChiRoutes,
-			reqMethodInSpanName:      cfg.RequestMethodInSpanName,
-			filters:                  cfg.Filters,
-			traceIDResponseHeaderKey: cfg.TraceIDResponseHeaderKey,
-			traceSampledResponseKey:  cfg.TraceSampledResponseKey,
-			publicEndpointFn:         cfg.PublicEndpointFn,
+			serverName:             serverName,
+			tracer:                 tracer,
+			propagators:            cfg.Propagators,
+			handler:                handler,
+			chiRoutes:              cfg.ChiRoutes,
+			reqMethodInSpanName:    cfg.RequestMethodInSpanName,
+			filters:                cfg.Filters,
+			traceResponseHeaderKey: cfg.TraceResponseHeaderKey,
+			publicEndpointFn:       cfg.PublicEndpointFn,
 		}
 	}
 }
 
 type traceware struct {
-	serverName               string
-	tracer                   oteltrace.Tracer
-	propagators              propagation.TextMapPropagator
-	handler                  http.Handler
-	chiRoutes                chi.Routes
-	reqMethodInSpanName      bool
-	filters                  []Filter
-	traceIDResponseHeaderKey string
-	traceSampledResponseKey  string
-	publicEndpointFn         func(r *http.Request) bool
+	serverName             string
+	tracer                 oteltrace.Tracer
+	propagators            propagation.TextMapPropagator
+	handler                http.Handler
+	chiRoutes              chi.Routes
+	reqMethodInSpanName    bool
+	filters                []Filter
+	traceResponseHeaderKey string
+	publicEndpointFn       func(r *http.Request) bool
 }
 
 type recordingResponseWriter struct {
@@ -180,10 +175,9 @@ func (tw traceware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx, span := tw.tracer.Start(ctx, spanName, spanOpts...)
 	defer span.End()
 
-	// put trace_id to response header only when [WithTraceIDResponseHeader] is used
-	if len(tw.traceIDResponseHeaderKey) > 0 && span.SpanContext().HasTraceID() {
-		w.Header().Add(tw.traceIDResponseHeaderKey, span.SpanContext().TraceID().String())
-		w.Header().Add(tw.traceSampledResponseKey, strconv.FormatBool(span.SpanContext().IsSampled()))
+	// put trace_id to response header only when WithTraceResponseHeaderKey is used
+	if len(tw.traceResponseHeaderKey) > 0 && span.SpanContext().HasTraceID() {
+		w.Header().Add(tw.traceResponseHeaderKey, span.SpanContext().TraceID().String())
 	}
 
 	// get recording response writer
