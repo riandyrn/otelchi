74bd3a59e547fe343f90d12621581fd7622bb4e5
Ilham Syahid S <ilhamsyahids@gmail.com>
2024-10-13 19:35:02 +0300

    revert metrics
    This reverts commit 7f4f21a80ed4a5cf92fec02ac2bf04938b802ec9.
    
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -5,39 +5,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/riandyrn/otelchi"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
-	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
+
+	stdout "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 	oteltrace "go.opentelemetry.io/otel/trace"
-
-	"github.com/riandyrn/otelchi"
 )
 
 var tracer oteltrace.Tracer
 
 func main() {
 	// initialize trace provider
-	tp, mp := initOtelProviders()
+	tp := initTracerProvider()
 	defer func() {
 		if err := tp.Shutdown(context.Background()); err != nil {
 			log.Printf("Error shutting down tracer provider: %v", err)
 		}
-		if err := mp.Shutdown(context.Background()); err != nil {
-			log.Printf("Error shutting down meter provider: %v", err)
-		}
 	}()
 	// set global tracer provider & text propagators
 	otel.SetTracerProvider(tp)
-	otel.SetMeterProvider(mp)
 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
 	// initialize tracer
 	tracer = otel.Tracer("mux-server")
@@ -45,28 +38,22 @@ func main() {
 	// define router
 	r := chi.NewRouter()
 	r.Use(otelchi.Middleware("my-server", otelchi.WithChiRoutes(r)))
-	r.HandleFunc("/users/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/users/{id:[0-9]+}", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "id")
 		name := getUser(r.Context(), id)
 		reply := fmt.Sprintf("user %s (id %s)\n", name, id)
 		w.Write(([]byte)(reply))
-	})
+	}))
 
 	// serve router
 	_ = http.ListenAndServe(":8080", r)
 }
 
-func initOtelProviders() (*sdktrace.TracerProvider, *sdkmetric.MeterProvider) {
-	traceExporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+func initTracerProvider() *sdktrace.TracerProvider {
+	exporter, err := stdout.New(stdout.WithPrettyPrint())
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	metricExporter, err := stdoutmetric.New(stdoutmetric.WithPrettyPrint())
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	res, err := resource.New(
 		context.Background(),
 		resource.WithAttributes(
@@ -78,15 +65,10 @@ func initOtelProviders() (*sdktrace.TracerProvider, *sdkmetric.MeterProvider) {
 		log.Fatalf("unable to initialize resource due: %v", err)
 	}
 	return sdktrace.NewTracerProvider(
-			sdktrace.WithSampler(sdktrace.AlwaysSample()),
-			sdktrace.WithBatcher(traceExporter),
-			sdktrace.WithResource(res),
-		), sdkmetric.NewMeterProvider(
-			sdkmetric.WithReader(sdkmetric.NewPeriodicReader(metricExporter,
-				sdkmetric.WithInterval(time.Second*30),
-			)),
-			sdkmetric.WithResource(res),
-		)
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
 }
 
 func getUser(ctx context.Context, id string) string {
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/stretchr/testify v1.9.0
 	go.opentelemetry.io/otel v1.30.0
-	go.opentelemetry.io/otel/metric v1.30.0
 	go.opentelemetry.io/otel/sdk v1.30.0
 	go.opentelemetry.io/otel/trace v1.30.0
 )
@@ -18,6 +17,7 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opentelemetry.io/otel/metric v1.30.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
